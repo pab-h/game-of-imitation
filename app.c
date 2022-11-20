5e4946fd2f787a3513fd3060c0dfa2c237ef6d20
pab-h <dev.pab.2020@gmail.com>
2022-11-20 11:38:26 -0300

    initial setup

---
+++ app.c
@@ -0,0 +1,10 @@
+#include "headers/constants.h"
+
+#include <stdio.h>
+
+int main() {
+
+    printf("O tamanho máximo da string será %d\n", MAX_STRING_LENGHT);
+
+    return 0;
+}
---
+++ headers/constants.h
@@ -0,0 +1,5 @@
+/* 
+* Segundo requirements.pdf, o máximo tamanho
+* de uma string será 100
+*/
+#define MAX_STRING_LENGHT 100
