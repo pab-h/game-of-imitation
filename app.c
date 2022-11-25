eaf91a0dd82ddd9eaf61bb6a6e4e50ea2774439d
pab-h <dev.pab.2020@gmail.com>
2022-11-25 16:00:12 -0300

    Funcao de codificar, decodificar e de teste

--- app.c
+++ app.c
@@ -4,12 +4,57 @@
 */
 #define MAX_STRING_LENGHT 100
 
-#include <stdio.h>
 #include <locale.h>
+#include <stdio.h>
+#include <string.h>
+
+void codificaTexto(char *texto, char *textoCodificado, const int deslocamento) {
+
+    while (*texto != '\0') {
+        *textoCodificado = *texto + deslocamento;
+
+        texto++;
+        textoCodificado++;
+    }
+
+    *textoCodificado = '\0';
+
+}
+
+void decodificaTexto(char *textoCodificado, char *texto, const int deslocamento) {
+
+    while (*textoCodificado != '\0') {
+        *texto = *textoCodificado - deslocamento;
+
+        texto++;
+        textoCodificado++;
+    }
+
+    *texto = '\0';
+
+}
+
+void teste() {
+    char texto[] = "voce acertou";
+    char textoCodificado[strlen(texto)];
+
+    codificaTexto(texto, textoCodificado, 5);
+
+    printf("Original: %s\n", texto);
+    printf("Codificado: %s\n", textoCodificado);
+
+    char textoDecodificado[strlen(texto)];
+
+    decodificaTexto(textoCodificado, textoDecodificado, 5);
+
+    printf("Decodificado: %s\n", textoDecodificado);
+}
 
 int main() {
 
     setlocale(LC_ALL, "");
+    
+    teste();
 
     return 0;
 }
